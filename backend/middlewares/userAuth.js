1d84629f68030e8b4771e981a256d5588566655e
applegrammer <web52995@gmail.com>
2024-09-26 13:54:45 +0200

    create user-auth middleware

---
+++ backend/middlewares/userAuth.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = process.env.SECRET_KEY;
+const User = require('../models/userModel');
+
+
+const userAuth = async (req, res, next) => {
+    try {
+        const authorizationHeader = req.headers.authorization;
+        if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
+            return res.status(401).json({ message: 'Authentication failed' });
+        }
+        const token = authorizationHeader.split(' ')[1];
+        const validToken = jwt.verify(token, SECRET_KEY);
+
+        const user = await User.findById(validToken.id);
+        if (!user) {
+            return res.status(401).json({ message: 'You are not authorized to complete the action' });
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: 'Invalid token' });
+    }
+}
+
+
+
+module.exports = userAuth;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/userController');
+const userAuth = require('../middlewares/userAuth');
 
 
 router.post('/signup', UserController.signup);
 router.post('/login', UserController.login);
-router.delete('/delete/:id', UserController.delete);
-router.put('/modify/:id', UserController.modify);
+router.delete('/delete/:id', userAuth, UserController.delete);
+router.put('/modify/:id', userAuth, UserController.modify);
 
 
 
