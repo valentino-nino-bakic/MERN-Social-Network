38d97ead1bcc6ea2a7279f75b8921737f21e725e
applegrammer <web52995@gmail.com>
2024-09-27 19:02:37 +0200

    create user login function containing API call to server

---
+++ frontend/src/api/constants.js
@@ -0,0 +1,8 @@
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+const API_ENDPOINTS = {
+    LOGIN: `${BASE_URL}/login`
+}
+
+
+export default API_ENDPOINTS;
---
+++ frontend/src/api/user.js
@@ -0,0 +1,31 @@
+import API_ENDPOINTS from './constants';
+
+const loginUser = async (usernameOrEmail, password) => {
+    const requestBody = {
+        usernameOrEmail,
+        password
+    }
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestBody)
+    }
+
+    try {
+        const response = await fetch(API_ENDPOINTS.LOGIN, options);
+        if (!response.ok) {
+            const data = await response.json();
+            throw new Error(data.message);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+
+export default loginUser;
