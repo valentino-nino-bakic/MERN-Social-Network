4803bdc59f163b1e8a352bbabf562691dac27f03
applegrammer <web52995@gmail.com>
2024-09-29 18:23:17 +0200

    create separate navbar for logged in user

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,38 +1,20 @@
 
 
-import { Navigate, useNavigate } from 'react-router-dom';
-import { jwtDecode } from 'jwt-decode';
+import { Navigate } from 'react-router-dom';
 
-import Button from '../components/Button';
 import useAuth from '../hooks/useAuth';
 
+
 const Profile = () => {
-    const navigate = useNavigate();
-
-    const { user, logout } = useAuth();
-
-    let username;
-    if (user) {
-        try {
-            username = jwtDecode(user).username;
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-    const handleLogout = async () => {
-        await logout();
-        navigate('/');
-    }
+    const { user } = useAuth();
 
     if (!user) {
         return <Navigate to='/' />
     }
+    
 
     return (
         <div>
-            <h1>Username: {username}</h1>
-            <Button type="button" onClick={handleLogout} className="btn btn-primary btn-block">Logout</Button>
             <p>user homepage</p>
             <p>user homepage</p>
             <p>user homepage</p>
