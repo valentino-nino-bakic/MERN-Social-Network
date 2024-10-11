937ae036a5fd907b896f594d5f42fe9ab02ecdec
applegrammer <web52995@gmail.com>
2024-10-11 22:46:31 +0200

    use post creation api function in context function

--- frontend/src/contexts/PostContext.js
+++ frontend/src/contexts/PostContext.js
@@ -1,5 +1,5 @@
 import { createContext, useState, useCallback } from 'react';
-import { getAllPosts } from '../api/post';
+import { getAllPosts, createPost } from '../api/post';
 
 
 const PostContext = createContext();
@@ -29,8 +29,25 @@ const PostProvider = ({ children }) => {
     }, []);
 
 
+
+    const createNewPost = useCallback(async (token, postData) => {
+        setLoading(true);
+        setError(null);
+        try {
+            const data = await createPost(token, postData);
+            setPosts((prev) => [data, ...prev]);
+            localStorage.setItem('allPosts', JSON.stringify([data, ...posts]));
+        } catch (error) {
+            setError(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }, [posts]);
+
+
+
     return (
-        <PostContext.Provider value={{ posts, getPosts, loading, error }}>
+        <PostContext.Provider value={{ posts, getPosts, createNewPost, loading, error }}>
             {children}
         </PostContext.Provider>
     )
