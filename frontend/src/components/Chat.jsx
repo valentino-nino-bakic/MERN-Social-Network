d7043f5b28ea9a5d6c845d2f7c8e62df34aeab33
applegrammer <web52995@gmail.com>
2024-12-28 17:47:08 +0100

    ui/ux improvements

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -9,7 +9,8 @@ const Chat = ({ user, selectedFriend, socket, getMessages, sendMessage /* handle
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
 
-    const handleSendMessage = () => {
+    const handleSendMessage = e  => {
+        e.preventDefault();
         sendMessage(jwtDecode(user).id, selectedFriend._id, message);
         setMessage('');
         setMessages([...messages, { senderId: jwtDecode(user).id, content: message }]);
@@ -41,22 +42,37 @@ const Chat = ({ user, selectedFriend, socket, getMessages, sendMessage /* handle
     return (
         <>
             {selectedFriend ? (
-                <div className="container-fluid d-flex flex-column">
+                <div className="container-fluid d-flex flex-column border shadow-sm rounded p-3">
+                    <div className="d-flex">
+                        <img src={selectedFriend.profileImageUrl} alt="profile" className="rounded-circle mx-2" style={{ height: '50px', width: '50px', objectFit: 'cover' }} />
+                        <p className="fw-bold">{selectedFriend.username}</p>
+                    </div>
+                    <hr />
                     {messages.map((message, index) => (
-                        <div key={index} className={`message ${message.senderId === jwtDecode(user).id ? 'align-self-end' : 'align-self-start'}`}>
-                            <p className="message-sender">{message.senderId === jwtDecode(user).id ? 'You' : selectedFriend.username}</p>
-                            <p className="message-content">{message.content}</p>
+                        <div key={index} className={`${message.senderId === jwtDecode(user).id ? 'align-self-end' : 'align-self-start'}`}>
+                            <div className="d-flex">
+                                {message.senderId === selectedFriend._id && <img src={selectedFriend.profileImageUrl} alt="profile" className="rounded-circle" style={{ height: '35px', width: '35px', objectFit: 'cover' }} />}
+                                <p className={`message ${message.senderId === jwtDecode(user).id ? 'my-message' : 'friend-message'}`}>{message.content}</p>
+                            </div>
                         </div>
                     ))}
-                    <div className="message-input">
-                        <input
-                            type="text"
-                            value={message}
-                            onChange={e => setMessage(e.target.value)}
-                            placeholder="Type a message..."
-                        />
-                        <button onClick={handleSendMessage}>Send</button>
-                    </div>
+                    <form onSubmit={handleSendMessage} className="message-input mt-5 container-fluid">
+                        <div className="row">
+                            <div className="col-md-10">
+                                <input
+                                    className="w-100 p-2 border rounded"
+                                    type="text"
+                                    value={message}
+                                    onChange={e => setMessage(e.target.value)}
+                                    placeholder="Type a message..."
+                                />
+                            </div>
+                            <div className="col-md-2 d-flex align-items-center justify-content-end">
+                               
+                                <button type="submit" className="btn btn-primary w-100">Send</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             ) : (
                 <div className="container">
--- frontend/src/pages/Messages.jsx
+++ frontend/src/pages/Messages.jsx
@@ -41,7 +41,7 @@ const Messages = () => {
 
     return (
         <>
-            <div className="container-fluid vh-100 m-5">
+            <div className="container vh-100 mt-5">
                 <div className="row">
                     <div className="col-md-4">
                         <div className="card shadow-sm mb-4">
